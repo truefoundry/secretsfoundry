e1ac8100c3f2530f135ef35df1592909fedc04d5
k-kumar-01 <kushalkumargupta4@gmail.com>
2021-10-04 23:24:27 +0530

    feat(jsii-lib): setup aws loader
    Signed-off-by: k-kumar-01 <kushalkumargupta4@gmail.com>

---
+++ jsii-lib/lib/Variables/loaders/AwsLoader.ts
@@ -0,0 +1,18 @@
+import BaseLoader from "./index";
+const os = require("os");
+const path = require("path");
+const { readFileSync } = require("fs");
+
+export default class AWSLoader implements BaseLoader {
+  public loadData(aws_variable: string): string {
+    const awsDir = path.join(path.join(os.homedir(), ".aws"), "credentials");
+
+    let fileContents: string[] = readFileSync(awsDir, "utf-8").split("\n");
+    for (const content of fileContents) {
+      if (content.startsWith(aws_variable)) {
+        return content.split("=", 2)[1];
+      }
+    }
+    throw Error(`Cannot find any AWS CREDENTIAL named ${aws_variable}`);
+  }
+}
