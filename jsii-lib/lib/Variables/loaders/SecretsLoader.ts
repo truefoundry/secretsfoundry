866e751cdc5020416ed8b9c4e2d04d94e941c80c
k-kumar-01 <kushalkumargupta4@gmail.com>
2021-10-04 23:28:12 +0530

    feat(jsii-lib): setup secrets-manager loader
    Signed-off-by: k-kumar-01 <kushalkumargupta4@gmail.com>

---
+++ jsii-lib/lib/Variables/loaders/SecretsLoader.ts
@@ -0,0 +1,49 @@
+import BaseLoader from "./index";
+
+const AWS = require("aws-sdk");
+// const Buffer = require('buffer');
+
+export default class SecretsLoader implements BaseLoader {
+  public async loadData(secrets_variable: string): Promise<string> {
+    const REGION_REGEX =
+      /^(us(-gov)?|ap|ca|cn|eu|sa)-(central|(north|south)?(east|west)?)-\d?/;
+    const NAME_REGEX = /^[\w\-]+$/;
+    const [region, secretName] = secrets_variable.split("/");
+
+    if (!REGION_REGEX.test(region)) {
+      throw new Error("Invalid Region provided");
+    }
+    if (!NAME_REGEX.test(secretName)) {
+      throw new Error("Improper Name provided");
+    }
+    try {
+      const data: { [key: string]: string } = await this.fetchData(
+        region,
+        secretName
+      );
+      return data.SecretString;
+    } catch (error) {
+      //@ts-ignore
+      throw new Error(`${error.code}: ${error.message}`);
+    }
+  }
+
+  private async fetchData(region: string, secretName: string): Promise<any> {
+    const client = new AWS.SecretsManager({
+      region: region,
+    });
+
+    return new Promise(function (success, reject) {
+      client.getSecretValue(
+        { SecretId: secretName },
+        function (err: any, data: { [key: string]: string }) {
+          if (err) {
+            reject(err);
+          } else {
+            success(data);
+          }
+        }
+      );
+    });
+  }
+}
