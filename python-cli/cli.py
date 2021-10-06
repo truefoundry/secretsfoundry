9bda861ae9498cf94276b9a472293dded5757b0a
tharun634 <tharun634@gmail.com>
2021-10-06 12:14:11 +0530

    addressing comments

--- python-cli/cli.py
+++ python-cli/cli.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 import os
 import shlex
 import subprocess
@@ -16,28 +15,24 @@ def run_shell_command(command, cwd=os.getcwd(), env=None, shell_mode=False):
     proc.communicate()
 
 
-# TODO(tharun634): uncomment this line, once cli goes into the python package
-# @click.version_option(__version__)
 @click.group()
+@click.version_option('0.1.0')
 def secretsfoundry_cli():
-    click.secho('secretsfoundry CLI', bold=True, fg='green')
+    """Secretsfoundry CLI"""
+    pass
 
 
-@secretsfoundry_cli.command(
-    help='load the corresponding staging .env file and run your command/script'
-)
-@click.option('--stage', type=click.STRING, help='Staging area to be used to run the command(s)', required=True)
+@secretsfoundry_cli.command()
+@click.option('--stage', type=click.STRING, help='Stage of the service', required=True)
 @click.option('-c', '--command', 'user_command', type=click.STRING, help='Single command to run')
-@click.option('-s', '--script', 'user_script', type=click.STRING, help='Multiple commands to run')
+@click.option('-s', '--script', 'user_script', type=click.STRING, help='Multiple commands to run like cd ~/ && ls')
 def run(stage, user_command, user_script):
+    """Run the process in command/script after injecting the environment variables"""
     if not (user_command or user_script):
         raise Exception('Either --script or --command is required, but none is found')
-    print('Selected stage: ', stage)
+    if user_command and user_script:
+        raise Exception('Cannot use both --script and --command at the same time')
     if user_command:
         run_shell_command(command=shlex.split(user_command))
     else:
         run_shell_command(command=user_script, shell_mode=True)
-
-
-if __name__ == '__main__':
-    secretsfoundry_cli()
