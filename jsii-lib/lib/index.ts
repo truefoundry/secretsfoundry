4bb21a021f2d6416525db3dd39ec9774f343e46f
k-kumar-01 <kushalkumargupta4@gmail.com>
2021-10-06 15:11:03 +0530

    feat: update function to extract Values
    Signed-off-by: k-kumar-01 <kushalkumargupta4@gmail.com>

--- jsii-lib/lib/index.ts
+++ jsii-lib/lib/index.ts
@@ -1,52 +1,28 @@
-import fs = require("fs");
 import variables from "./variables";
 
 export class SecretsFoundry {
-  public async readFile(filePath: string) {
-    // specifying an encoding returns a string instead of a buffer
-    const inputBuffer: fs.PathLike = fs.readFileSync(filePath, "utf8");
-
-    const NEWLINE = "\n";
-    const RE_INI_KEY_VAL = /^\s*([\w.-]+)\s*=\s*(.*)?\s*$/;
-    const RE_NEWLINES = /\\n/g;
-    const NEWLINES_MATCH = /\r\n|\n|\r/;
-
-    let obj: { [key: string]: string | Promise<string> } = {};
-
-    // convert Buffers before splitting into lines and processing
-    const lines = inputBuffer.toString().split(NEWLINES_MATCH);
-    for (const [idx, line] of lines.entries()) {
-      // matching "KEY' and 'VAL' in 'KEY=VAL'
-      const keyValueArr = line.match(RE_INI_KEY_VAL);
-      if (keyValueArr != null) {
-        let key = keyValueArr[1];
-        // default undefined or missing values to empty string
-        let val: string = keyValueArr[2] || "";
-        const end = val.length - 1;
-        const isDoubleQuoted = val[0] === '"' && val[end] === '"';
-        const isSingleQuoted = val[0] === "'" && val[end] === "'";
-        // if single or double quoted, remove quotes
-        if (isSingleQuoted || isDoubleQuoted) {
-          val = val.substring(1, end);
-          // if double quoted, expand newlines
-          if (isDoubleQuoted) {
-            val = val.replace(RE_NEWLINES, NEWLINE);
-          }
-        } else {
-          // remove surrounding whitespace
-          val = val.trim();
-        }
-        try {
-          obj[key] = await new variables().populate(val);
-        } catch (error) {
-          throw new Error(error as string);
-        }
-      } else {
-        throw new Error(
-          `did not match key and value when parsing line ${idx + 1}: ${line}`
-        );
-      }
+  /**
+   * Extracts the values read from a file and populates the variables present
+   * @param data An object/dict containing key,value (both strings)
+   * @returns Object/dict containing key and corresponding populated value
+   */
+  public async extractValues(data: { [key: string]: string }) {
+    let keys: string[] = [];
+    let values: Promise<string>[] = [];
+    let extractedValues: { [key: string]: string | Promise<string> } = {};
+    for (const key in data) {
+      keys.push(key);
+      values.push(new variables().populate(data[key]));
     }
-    return obj;
+    try {
+      const results = await Promise.all(values);
+      keys.forEach((key, idx) => {
+        // assign the key-value pair
+        extractedValues[key] = results[idx];
+      });
+      return extractedValues;
+    } catch (error) {
+      throw new Error(error as string);
+    }
   }
 }
