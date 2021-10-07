ac7e87277ccf068a94fdb9612f1749e111bc16b8
Abhishek Choudhary <abhichoudhary06@gmail.com>
2021-10-08 00:00:17 +0530

    Added loader.ts file

---
+++ src/loaders/loader.ts
@@ -0,0 +1,29 @@
+// a custom made separator for splitting values
+
+export default abstract class Loader {
+  /**
+   * This function resolves the value of the input variable by contacting
+   * the secret manager store and getting the value
+   * @param args
+   */
+  public abstract resolve(...args: string[]): Promise<string>;
+
+  /**
+   * This function returns true if the loader knows how to get the values for the
+   * variable from the source mentioned in the input.
+   * @param value The string to be resolved of the format provider(args):/path-to-key
+   */
+  public abstract canResolve(variable: string): boolean;
+
+  getArgsFromStr(argsStr: string): Record<string, string> {
+    const argsMap: Record<string, string> = {};
+    if (argsStr) {
+      const args = argsStr.trim().split(',');
+      for (const arg in args) {
+        const keyValue = arg.trim().split('=');
+        argsMap[keyValue[0]] = keyValue[1];
+      }
+    }
+    return argsMap;
+  }
+}
