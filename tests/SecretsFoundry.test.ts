b5db9e82ba778a5947ea87f27d3f3e8bfbd005d0
Blaine <rishi1998@gmail.com>
2021-10-08 11:54:55 +0530

    Fix: Use only one foundry instance

--- tests/SecretsFoundry.test.ts
+++ tests/SecretsFoundry.test.ts
@@ -18,8 +18,7 @@ class DummyLoader extends Loader {
 }
 
 // Reusable constants
-const foundryWithNoLoaders = new SecretsFoundry([]);
-const foundryWithDummyLoaders = new SecretsFoundry([
+const foundry = new SecretsFoundry([
   new DummyLoader('sourceA'),
   new DummyLoader('sourceB'),
   new DummyLoader('sourceC'),
@@ -47,43 +46,21 @@ const completeBaseRecord: Record<string, string> = {
 
 describe('SecretsFoundry', () => {
   it('Should resolve standard vars', async () => {
-    const response = await foundryWithNoLoaders.dotenvExpand(baseRecord);
+    const response = await foundry.dotenvExpand(baseRecord);
     expect(response).toStrictEqual(envVars);
   });
 
   it('Should resolve value for dummy loaders', async () => {
     const response = await Promise.all([
-      foundryWithDummyLoaders.resolveVar(
+      foundry.resolveVar(
         'sourceA:hello_world',
         envVars
       ),
-      foundryWithDummyLoaders.resolveVar(
+      foundry.resolveVar(
         'sourceB:hello_world',
         envVars
       ),
-      foundryWithDummyLoaders.resolveVar(
-        'sourceC:hello_world',
-        envVars
-      )
-    ]);
-    expect(response).toStrictEqual([
-      'resolved_by_sourceA',
-      'resolved_by_sourceB',
-      'resolved_by_sourceC'
-    ]);
-  });
-
-  it('Should resolve value for dummy loaders', async () => {
-    const response = await Promise.all([
-      foundryWithDummyLoaders.resolveVar(
-        'sourceA:hello_world',
-        envVars
-      ),
-      foundryWithDummyLoaders.resolveVar(
-        'sourceB:hello_world',
-        envVars
-      ),
-      foundryWithDummyLoaders.resolveVar(
+      foundry.resolveVar(
         'sourceC:hello_world',
         envVars
       )
@@ -96,7 +73,7 @@ describe('SecretsFoundry', () => {
   });
 
   it('Should resolve vars with both simple and dummy foundry vars', async () => {
-    const response = await foundryWithDummyLoaders.dotenvExpand(completeBaseRecord);
+    const response = await foundry.dotenvExpand(completeBaseRecord);
     expect(response).toStrictEqual({
       ...envVars,
       'A_Record': 'resolved_by_sourceA',
