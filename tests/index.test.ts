9a1bc0c01bb485a8401b525690ce05a9ee0bb6cc
k-kumar-01 <kushalkumargupta4@gmail.com>
2021-10-10 19:27:13 +0530

    tests: update existing tests; add more tests
    Signed-off-by: k-kumar-01 <kushalkumargupta4@gmail.com>

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,49 +1,64 @@
 import Utils, { Options } from '../src/utils';
 
 describe('ValidatesInput:', () => {
-  const options: Options = {
-    stage: undefined,
-    command: 'test',
-    script: undefined,
-    path: undefined,
-  };
   it('should validate input(only command)', () => {
-    const result = Utils.validateInput(options);
+    const result = Utils.validateInput({ command: 'test' });
     expect(result).toBeTruthy();
   });
 
-  options.stage = 'dev';
   it('should validate input(command + stage)', () => {
-    const result = Utils.validateInput(options);
+    const result = Utils.validateInput({ command: 'test', stage: 'dev' });
     expect(result).toBeTruthy();
   });
 
-  options.path = 'src';
-  options.stage = undefined;
   it('should validate input(command + path)', () => {
-    const result = Utils.validateInput(options);
+    const result = Utils.validateInput({
+      command: 'test',
+      path: 'src',
+    });
     expect(result).toBeTruthy();
   });
 
-  options.script = 'test';
-  options.command = undefined;
-  options.path = undefined;
   it('should validate input(only script)', () => {
-    const result = Utils.validateInput(options);
+    const result = Utils.validateInput({
+      script: 'test',
+    });
     expect(result).toBeTruthy();
   });
 
-  options.stage = 'dev';
-  it('should validate input(command + stage)', () => {
-    const result = Utils.validateInput(options);
+  it('should validate input(script + stage)', () => {
+    const result = Utils.validateInput({ script: 'test', stage: 'dev' });
     expect(result).toBeTruthy();
   });
 
-  options.path = 'src/utils';
-  options.stage = undefined;
   it('should validate input(script + path)', () => {
-    const result = Utils.validateInput(options);
+    const result = Utils.validateInput({
+      script: 'test',
+      path: 'src',
+    });
     expect(result).toBeTruthy();
+  });
+
+  it('should fail(both command and script)', () => {
+    const mockExit = jest.spyOn(process, 'exit').mockImplementation((code) => {
+      throw new Error('process.exit: ' + code);
+    });
+    expect(() => {
+      Utils.validateInput({ command: 'test', script: 'test' });
+    }).toThrow();
+    expect(mockExit).toHaveBeenCalledWith(-1);
+    mockExit.mockRestore();
+  });
+
+  it("should fail(folder doesn't exist )", () => {
+    const mockExit = jest.spyOn(process, 'exit').mockImplementation((code) => {
+      throw new Error('process.exit: ' + code);
+    });
+    expect(() => {
+      Utils.validateInput({ path: 'srt' });
+    }).toThrow();
+    expect(mockExit).toHaveBeenCalledWith(-1);
+    mockExit.mockRestore();
   });
 });
 
