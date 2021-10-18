29adab83a0e11363aecc80fa88d4450c8eb2cdd0
Kushal Kumar <59891164+K-Kumar-01@users.noreply.github.com>
2021-10-18 19:00:14 +0530

    Testing utils (#42)
    * refactor: move functions to utils; provide default stage in extractValues
    
    Signed-off-by: k-kumar-01 <kushalkumargupta4@gmail.com>
    
    * tests: add tests for utils
    
    Signed-off-by: k-kumar-01 <kushalkumargupta4@gmail.com>
    
    * refactor: make utils a class with static functions
    
    Signed-off-by: k-kumar-01 <kushalkumargupta4@gmail.com>
    
    * refactor(utils): move spawnChld inside class
    
    Signed-off-by: k-kumar-01 <kushalkumargupta4@gmail.com>
    
    * tests: update existing tests; add more tests
    
    Signed-off-by: k-kumar-01 <kushalkumargupta4@gmail.com>

---
+++ src/utils.ts
@@ -0,0 +1,82 @@
+import chalk from 'chalk';
+import fs from 'fs';
+import { spawn } from 'child_process';
+
+export interface Options {
+  stage?: string;
+  command?: string;
+  script?: string;
+  path?: string;
+}
+
+export default class Utils {
+  /**
+   * Executes a multi-command using shell.
+   * @param command command to be executed
+   * @returns arguments array to be passed to child process
+   *
+   * When multiple commands are chained together, we need to run
+   * them in shell. However, the shell will be different for Windows
+   * and Unix based systems. So we decide the shell command accordingly.
+   */
+  static getScriptArgs(script: string): string[] {
+    let args: string[] = [];
+
+    if (process.platform === 'win32') {
+      args = ['cmd', '/C', script];
+      return args;
+    }
+    // Figure out the shell in Unix
+    const shells: string[] = [
+      '/bash',
+      '/dash',
+      '/fish',
+      '/zsh',
+      '/ksh',
+      '/csh',
+      '/tcsh',
+    ];
+    args = ['sh', '-c', script];
+    const envShell = process.env.SHELL as string;
+    for (const shell of shells) {
+      if (shell.endsWith(envShell)) {
+        args[0] = envShell;
+      }
+    }
+    return args;
+  }
+
+  /**
+   * Log Error and Exit
+   * @param message   message to be logged in case of error
+   */
+  static logErrorAndExit(message: string): void {
+    console.log(chalk.red(`ERROR: ${message}`));
+    process.exit(-1);
+  }
+
+  static validateInput(options: Options): boolean {
+    if (options.path && options.path.trim()) {
+      options.path = options.path.trim();
+      if (!fs.existsSync(options.path)) {
+        this.logErrorAndExit(`Config folder, ${options.path} does not exist`);
+      }
+    }
+    if (options.script && options.command) {
+      this.logErrorAndExit(
+        'Cannot use both --script and --command at the same time'
+      );
+    }
+    if (options.command && !options.command.trim()) {
+      this.logErrorAndExit('Command cannot be empty');
+    }
+    if (options.script && !options.script.trim()) {
+      this.logErrorAndExit('Script cannot be empty');
+    }
+
+    return true;
+  }
+  static runChildProcess(cmd: string, args: string[]): void {
+    spawn(cmd, args, { stdio: 'inherit' });
+  }
+}
---
+++ tests/index.test.ts
@@ -0,0 +1,78 @@
+import Utils, { Options } from '../src/utils';
+
+describe('ValidatesInput:', () => {
+  it('should validate input(only command)', () => {
+    const result = Utils.validateInput({ command: 'test' });
+    expect(result).toBeTruthy();
+  });
+
+  it('should validate input(command + stage)', () => {
+    const result = Utils.validateInput({ command: 'test', stage: 'dev' });
+    expect(result).toBeTruthy();
+  });
+
+  it('should validate input(command + path)', () => {
+    const result = Utils.validateInput({
+      command: 'test',
+      path: 'src',
+    });
+    expect(result).toBeTruthy();
+  });
+
+  it('should validate input(only script)', () => {
+    const result = Utils.validateInput({
+      script: 'test',
+    });
+    expect(result).toBeTruthy();
+  });
+
+  it('should validate input(script + stage)', () => {
+    const result = Utils.validateInput({ script: 'test', stage: 'dev' });
+    expect(result).toBeTruthy();
+  });
+
+  it('should validate input(script + path)', () => {
+    const result = Utils.validateInput({
+      script: 'test',
+      path: 'src',
+    });
+    expect(result).toBeTruthy();
+  });
+
+  it('should fail(both command and script)', () => {
+    const mockExit = jest.spyOn(process, 'exit').mockImplementation((code) => {
+      throw new Error('process.exit: ' + code);
+    });
+    expect(() => {
+      Utils.validateInput({ command: 'test', script: 'test' });
+    }).toThrow();
+    expect(mockExit).toHaveBeenCalledWith(-1);
+    mockExit.mockRestore();
+  });
+
+  it("should fail(folder doesn't exist )", () => {
+    const mockExit = jest.spyOn(process, 'exit').mockImplementation((code) => {
+      throw new Error('process.exit: ' + code);
+    });
+    expect(() => {
+      Utils.validateInput({ path: 'srt' });
+    }).toThrow();
+    expect(mockExit).toHaveBeenCalledWith(-1);
+    mockExit.mockRestore();
+  });
+});
+
+describe('GetScriptArgs:', () => {
+  it('should pass', () => {
+    const script = 'npm i && npm --version';
+    const args = Utils.getScriptArgs(script);
+    if (process.platform === 'win32') {
+      expect(args[0]).toEqual('cmd');
+      expect(args[1]).toEqual('/C');
+      expect(args[2]).toEqual(script);
+    } else {
+      expect(args[1]).toEqual('-c');
+      expect(args[2]).toEqual(script);
+    }
+  });
+});
