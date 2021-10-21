b169f1401af529a7b4f30673211eed8da9b794ae
Blaine <rishi1998@gmail.com>
2021-10-21 15:12:22 +0530

    Chore: Add node-vault mock

--- tests/loaders/VaultLoader.test.ts
+++ tests/loaders/VaultLoader.test.ts
@@ -1,4 +1,8 @@
 import vaultLoader from '../../src/loaders/VaultLoader';
+import nodeVault from 'node-vault';
+jest.mock('node-vault');
+
+console.log(nodeVault)
 
 describe('vaultLoader', () => {
   it('should resolve', () => {
@@ -12,4 +16,21 @@ describe('vaultLoader', () => {
     const isResolved = loader.canResolve('vaut:random-name');
     expect(isResolved).not.toBeTruthy();
   });
+
+  it('should resolve vault secrets', async () => {
+    const loader = new vaultLoader();
+    process.env.ROLE_ID = 'Role';
+    process.env.SECRET_ID = 'Secret';
+    expect(
+      await loader.resolve(
+        'vault(endpoint_url=someEndpoint):random-name'
+      )
+    ).toStrictEqual(JSON.stringify('Role-random-name'));
+    process.env.VAULT_ENDPOINT_URL = 'someEndpoint'
+    expect(
+      await loader.resolve(
+        'vault:random-name'
+      )
+    ).toStrictEqual(JSON.stringify('Role-random-name'))
+  })
 });
