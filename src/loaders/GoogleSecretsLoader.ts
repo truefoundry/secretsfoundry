cf66ff7f143dde99c710c1c176809b38202a7f3f
Srihari Radhakrishna <srihari@Sriharis-MacBook-Pro.local>
2022-10-04 18:25:24 +0530

    add gcp to loaders

--- src/loaders/GoogleSecretsLoader.ts
+++ src/loaders/GoogleSecretsLoader.ts
@@ -3,20 +3,18 @@ import Loader from './loader';
 import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 
 /**
- * GoogleSecretsLoader loads the secret from AWSSecretsManager.
+ * GoogleSecretsLoader loads the secret from Google Secrets Manager.
  * This loader accepts pattern of the format
- * aws-secrets(params):/key-to-param
+ * gcp-secrets:key-to-secret-version
  *
  * Requirements:
  *
- * AWS credentials from the environment variables
- * AWS_ACCESS_KEY_ID
- * AWS_SECRET_ACCESS_KEY.
+ * Authenticate requests to GCP using the environment variables
+ * GOOGLE_APPLICATION_CREDENTIALS
  *
- * It also falls back to looking up the credentials in local
- * aws config directory if it cannot find the variables set.
+ * Usage: export GOOGLE_APPLICATION_CREDENTIALS=KEY_PATH
+ * Replace KEY_PATH with the path of the JSON file that contains your service account key.
  *
- * @param region: AWS region to get the parameter from
  */
 export default class GoogleSecretsLoader extends Loader {
   private static PATTERN =
@@ -35,23 +33,12 @@ export default class GoogleSecretsLoader extends Loader {
       );
     }
 
-    const argsStr = groups[2]; // args
     const secretName = groups[3]; // path to param
-    const args = this.getArgsFromStr(argsStr);
 
     const client = new SecretManagerServiceClient();
-    const [secret] = await client.getSecret({
+    const [secret] = await client.accessSecretVersion({
       name: secretName,
-    });
-    console.log(secret);
-
-    // get secret from AWS Secrets Manager
-    // const result = await client
-    //   .getSecretValue({ SecretId: secretName })
-    //   .promise();
-    // if ('SecretString' in result) {
-    //   return result.SecretString as string;
-    // }
-    // throw new Error('No SecretString in the Secret: `${secretsVariable}`');
+    }, );
+    return secret.payload?.data?.toString() as string;
   }
 }
