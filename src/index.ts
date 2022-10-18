71dcf049317bb92017a779c22849ee723e74dac6
sunanda690 <58281090+sunanda690@users.noreply.github.com>
2022-10-18 18:03:39 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import { Command } from 'commander';
 import { SecretsFoundry } from './SecretsFoundry';
 import { Loaders } from './loaders';
 import Utils, { Options } from './utils';
-import pjson = require('./../package.json');
+import packageJson = require('./../package.json');
 
 const SUPPORTED_FORMATS = ['yaml', 'json', 'env', 'yml']
 
 const program = new Command();
 program
-  .version(pjson.version)
+  .version(packageJson.version)
   .command('run')
   .option('--stage <string>', 'Stage of the service', '')
   .option('-i, --input <string>', 'Input file containing variables (.env/json/yaml)')
